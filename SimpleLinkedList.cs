57a416c5b857595f0ca9bd354deef2e3bda46f48
bhumika1111 <bhoyarpatil@434>
2022-08-13 23:15:07 +0530

    * Simple LinkedList program File*

---
+++ SimpleLinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList.cs
+{
+    /// <summary>
+    /// Here We Are Creating Simple Link List .
+    /// </summary>
+  public static class SimpleLinkedList
+    {
+        public static void LinkedlistDisplay()
+        {
+            List<int> list = new List<int>();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+
+            foreach(int ele in list)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("Count List Of Elements:" + list.Count);
+        } 
+    }
+}
