b03f3d21727cbb344652628dc32e9aa19de68a74
bhumika1111 <bhoyarpatil@434>
2022-08-13 23:10:48 +0530

    * Added Linked List program File*

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkedList.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("::::::Welcome To Linked List Program::::::::");
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::");
+
+            Console.WriteLine("****Output For Simple LinkedList For 56-> 30-> 70->*****");
+            SimpleLinkedList.LinkedlistDisplay();
+        }
+    }
+}
