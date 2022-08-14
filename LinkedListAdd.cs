6fc4d9aa3c9dbd5343cdb8d7553e0e1a0ff889d4
bhumika1111 <bhoyarpatil@434>
2022-08-14 12:21:01 +0530

    *Add Data To Linkedlist *

---
+++ LinkedListAdd.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linkedlist
+{
+    /// <summary>
+    /// Here We Are Creating Linked List For Adding Data.
+    /// </summary>
+  public  class LinkedListAdd
+    {
+       public Node head;
+
+        //Insert The a new Node at front of the list.
+            public void Add(int newdata)
+            {
+               //Allocate the Node and put in the data.
+                Node newnode = new Node(newdata);
+               //Make next of the new Node as head.
+                newnode.next = head;
+             //Move the head to point to new Node.
+                head = newnode;
+            }
+             //This function display content of linked list starting from the given Node.
+            public void Display()
+            {
+                Node tnode = head;
+                while (tnode != null)
+                {
+                    Console.Write( "{0}->", tnode.data );
+                    tnode = tnode.next;
+                }
+            }
+        }
+}
