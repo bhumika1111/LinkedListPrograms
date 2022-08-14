3db975e489fcd38a6dffb1c999d04612ead2ea57
bhumika1111 <bhoyarpatil@434>
2022-08-14 12:15:53 +0530

    *Added LinkedList Program File*

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Linkedlist
+{
+    //Driver Code
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Output For LinkedList ****");
+            Console.WriteLine(":::::::::::::::::::::::::::::::::");
+            // Create Object Of Implementing Class.
+            LinkedListAdd Linkedlist = new LinkedListAdd();
+            //Insert 70 At Staring.
+            Linkedlist.Add(70);
+            //Inserted 30 To 70.
+            Linkedlist.Add(30);
+            //Inserted 56 To 30
+            Linkedlist.Add(56);
+            //Display The Linkedlist.
+            Linkedlist.Display();
+        }
+    }
+}
