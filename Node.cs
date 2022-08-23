6f95c6484232324a5d413e170818af1d86ebe0f7
bhumika1111 <bhoyarpatil@434>
2022-08-23 22:41:28 +0530

    *Check Node File*

---
+++ Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListNew
+{
+       public class Node
+        {
+
+            public int data;
+            public Node Next;
+
+            //constructor of Node class
+            public Node(int data)
+            {
+                this.data = data;
+                Next = null;
+            }
+        }
+}
